a0d52cf4fc91b74e78dde0cd986b2814c6386f81
Vagner H <vh3rd7@gmail.com>
2020-08-02 18:25:14 -0300

    added team model

---
+++ hackathonTeamSearch/Models/Team.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hackathonTeamSearch.Models
+{
+    public class Team
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string Name { get; set; }
+
+    }
+}
