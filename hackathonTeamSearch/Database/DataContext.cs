39b1ad1bfdc21585a773787ab543ebdea1d0517c
Vagner H <vh3rd7@gmail.com>
2020-08-03 18:31:04 -0300

    add connection to the database

---
+++ hackathonTeamSearch/Database/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using hackathonTeamSearch.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hackathonTeamSearch.Database
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Team> Teams { get; set; }
+    }
+}
